2301cd2e4edff25a5b45386db38deb6b98fabc17
BeefOnWeck <79946890+BeefOnWeck@users.noreply.github.com>
2023-07-27 06:32:12 -0400

    Part 4: Tiles and components

---
+++ board_plugin/src/components/bomb_neighbor.rs
@@ -0,0 +1,13 @@
+use bevy::prelude::Component;
+
+use bevy::reflect::Reflect;
+use bevy_inspector_egui::prelude::ReflectInspectorOptions;
+use bevy_inspector_egui::InspectorOptions;
+
+#[cfg_attr(feature = "debug", derive(Reflect, InspectorOptions))]
+#[cfg_attr(feature = "debug", reflect(InspectorOptions))]
+#[derive(Debug, Copy, Clone, Ord, PartialOrd, Eq, PartialEq, Hash, Component)]
+pub struct BombNeighbor {
+    /// Number of neighbor bombs
+    pub count: u8,
+}
